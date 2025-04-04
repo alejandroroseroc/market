e5b52e9f881088075f570f13240e454ba49a8cbb
Diego Alejandro Rosero <diego.roseroc@campusucc.edu.co>
2025-04-03 21:34:27 -0500

    Subiendo entidad OrdenItem

---
+++ market/src/main/java/com/example/market/infraestructure/entity/OrdenItem.java
@@ -0,0 +1,78 @@
+package com.example.market.infraestructure.entity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "orden_items")
+public class OrdenItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Integer cantidad;
+    private Double precioUnitario;
+
+    @ManyToOne
+    @JoinColumn(name = "producto_id", nullable = false)
+    private Producto producto;
+
+    @ManyToOne
+    @JoinColumn(name = "orden_id", nullable = false)
+    private Orden orden;
+
+    public OrdenItem() {}
+
+    public OrdenItem(Integer cantidad, Double precioUnitario, Producto producto, Orden orden) {
+        this.cantidad = cantidad;
+        this.precioUnitario = precioUnitario;
+        this.producto = producto;
+        this.orden = orden;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(Integer cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public Double getPrecioUnitario() {
+        return precioUnitario;
+    }
+
+    public void setPrecioUnitario(Double precioUnitario) {
+        this.precioUnitario = precioUnitario;
+    }
+
+    public Producto getProducto() {
+        return producto;
+    }
+
+    public void setProducto(Producto producto) {
+        this.producto = producto;
+    }
+
+    public Orden getOrden() {
+        return orden;
+    }
+
+    public void setOrden(Orden orden) {
+        this.orden = orden;
+    }
+    
+}
